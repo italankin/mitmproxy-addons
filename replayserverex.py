e3afb52d5ef54690f4f5405415b8cbaddec2ec3d
Igor Talankin <italankin@gmail.com>
2019-06-11 13:04:08 +0500

    Add replayserverex.py

---
+++ replayserverex.py
@@ -0,0 +1,81 @@
+import typing
+import urwid
+
+from mitmproxy import command
+from mitmproxy import flow
+from mitmproxy import ctx
+from mitmproxy import exceptions
+from mitmproxy import io
+from mitmproxy.addons import serverplayback
+from mitmproxy.tools.console import overlay
+import mitmproxy.types
+
+
+class ReplayServerEx:
+    @command.command("replay.server.add")
+    def add_flows(self, flows: typing.Sequence[flow.Flow]) -> None:
+        """
+            Replay server responses from flows.
+        """
+        addon = self._find_addon()
+        for i in flows:
+            if i.response:
+                l = addon.flowmap.setdefault(addon._hash(i), [])
+                l.append(i)
+        ctx.master.addons.trigger("update", [])
+        ctx.log.alert("Added %d flows." % len(flows))
+
+    @command.command("replay.server.file.add")
+    def load_file(self, path: mitmproxy.types.Path) -> None:
+        """
+            Append server responses from file.
+        """
+        try:
+            flows = io.read_flows_from_paths([path])
+        except exceptions.FlowReadException as e:
+            raise exceptions.CommandError(str(e))
+        self.add_flows(flows)
+
+    @command.command("replay.server.list")
+    def list_flows(self) -> None:
+        """
+            Show server responses list.
+        """
+        recorded_flows = []
+        for flows in self._find_addon().flowmap.values():
+            for f in flows:
+                recorded_flows.append(f)
+
+        if len(recorded_flows) == 0:
+            ctx.log.alert("No flows.")
+            return
+
+        choices = []
+        for flow in recorded_flows:
+            c = FlowChoice(flow.request.url)
+            c.flow = flow
+            choices.append(c)
+
+        def callback(opt):
+            flow = opt.flow
+            if flow not in ctx.master.view:
+                ctx.log.alert("Not found.")
+            else:
+                ctx.master.view.focus.flow = opt.flow
+
+        ctx.master.overlay(
+            overlay.Chooser(ctx.master, "Flows", choices, "", callback)
+        )
+
+    def _find_addon(self) -> serverplayback.ServerPlayback:
+        addon = ctx.master.addons.get("serverplayback")
+        if addon == None:
+            raise ValueError("Cannot find addon 'serverplayback'")
+        return addon
+
+class FlowChoice(str):
+    pass
+
+addons = [
+    ReplayServerEx()
+]
