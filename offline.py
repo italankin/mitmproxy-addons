97c98e05fb3d48a77e690ba6aeff2baa42da9235
Igor Talankin <italankin@gmail.com>
2019-06-18 10:59:47 +0500

    Add offline.py

---
+++ offline.py
@@ -0,0 +1,27 @@
+from mitmproxy import http
+from mitmproxy import ctx
+
+
+class Offline:
+    def __init__(self):
+        self.offline = False
+
+    def load(self, loader):
+        loader.add_option(
+            "offline", bool, False,
+            "Kill HTTP CONNECT requests",
+        )
+
+    def configure(self, updates):
+        if "offline" in updates:
+            value = ctx.options.offline
+            if value is not None:
+                self.offline = value
+
+    def http_connect(self, flow):
+        if self.offline:
+            flow.kill()
+
+addons = [
+    Offline()
+]
