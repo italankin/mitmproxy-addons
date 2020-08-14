d93ba0ce88930681c8df2a6a09f44fe4b01b27f5
Igor Talankin <italankin@gmail.com>
2020-08-15 00:48:13 +0500

    Added error handling for options in sleeper.py

--- sleeper.py
+++ sleeper.py
@@ -1,36 +1,44 @@
 import time
+from typing import Optional
 
 from mitmproxy import ctx
 from mitmproxy import flowfilter
 from mitmproxy.script import concurrent
+from mitmproxy.exceptions import OptionsError
 
+
+matchall = flowfilter.parse(".")
 
 class Sleeper:
     def __init__(self):
-        self.filter: flowfilter.TFilter = None
+        self.filter: Optional[flowfilter.TFilter] = matchall
 
     def load(self, loader):
         loader.add_option(
-            "sleep", int, 0,
+            "sleep", Optional[int], None,
             "Delay client requests (milliseconds)",
         )
         loader.add_option(
-            "sleep_filter", str, "",
-            "Delay only matching flows"
+            "sleep_filter", Optional[str], None,
+            "Apply delay to flows which match the filter"
         )
 
     def configure(self, updates):
         if "sleep" in updates:
-            value = ctx.options.sleep
-            if value is not None and value < 0:
-                raise ValueError("'sleep' must be >= 0")
+            sleep = ctx.options.sleep
+            if sleep and sleep < 0:
+                raise OptionsError("'sleep' must be >= 0")
         if "sleep_filter" in updates:
-            self.filter = flowfilter.parse(ctx.options.sleep_filter)
+            filt_str = ctx.options.sleep_filter
+            filt = matchall if not filt_str else flowfilter.parse(filt_str)
+            if not filt:
+                raise OptionsError("Invalid filter expression: %s" % filt_str)
+            self.filter = filt
 
     @concurrent
     def request(self, flow):
         delay = ctx.options.sleep
-        if delay > 0 and flowfilter.match(self.filter, flow):
+        if delay and delay > 0 and flowfilter.match(self.filter, flow):
             time.sleep(delay / 1000)
 
 
